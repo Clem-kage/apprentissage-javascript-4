a0ec0082fd42c925d6dde59d3c3dbb4c97a10783
clemk <clemkfr@gmail.com>
2022-11-09 20:18:33 +0100

    ajout affichage

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let nom = document.querySelector('input[id="name"]');
 let valeur = document.querySelector('input[id="value"]');
 let formulaire  = document.querySelector('form');
+let bouttonAfficher = document.querySelector('.display-cookie-btn');
 let liste = document.querySelector('ul[class="cookies-list"]')
-
-
+let listeStatus = false;
 let loc = JSON.parse(localStorage.getItem('loc'))
 let tab = []
+let done = false
+
 let currentTab = null
 function init() {
     if(!loc){
@@ -17,10 +19,19 @@ function init() {
 }
 
 init()
+  
+let suppLastList = ()=>{
+    while (liste.hasChildNodes()) {
+        liste.removeChild(liste.firstChild);
+      }
+}
 
     let enregTab = (tab, res)=>{
       tab.push(res)
       localStorage.setItem('loc', JSON.stringify(tab))
+      //------------------------------
+      done = false
+      suppLastList()
     }
 
     let enregistrer = (e)=>{
@@ -33,8 +44,36 @@ init()
         enregTab(tab, result);
     }
 
-    let afficher =()=>{
-        console.log(tab);
+    createList = (data)=>{
+        let element = document.createElement('li');
+        element.textContent = `nom: ${data.nom}    valeur: ${data.valeur}`;
+        element.style.listStyle = 'none';
+        liste.appendChild(element);
     }
 
+    let stautusManager = ()=>{
+        if(!listeStatus){
+           listeStatus = true;
+           liste.style.display = "block";
+           if(!done){
+           afficher();
+           }
+        }
+        else{
+           listeStatus = false
+           liste.style.display = "none";
+        }
+    }
+
+    let afficher =()=>{
+            console.log('afficher')
+            console.log(liste);
+            tab.forEach(el => {
+                createList(el)
+            });
+            done = true
+    }
+
+
     formulaire.addEventListener("submit", enregistrer);
+    bouttonAfficher.addEventListener('click', stautusManager)
